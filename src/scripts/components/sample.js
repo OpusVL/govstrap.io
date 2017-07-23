f4ca8a3de5cb0060d1ab0c205ae8596aa8f816f5
JohnMica <liviumica@me.com>
2017-07-24 00:21:25 +0100

    gulp-done
    finalized the autocompile // server with gulp;
    
    restructured the content to optimize development by modules

---
+++ src/scripts/components/sample.js
@@ -0,0 +1,74 @@
+// (($, window, document) => {
+//   /**
+//    * This is a description for Module object.
+//    *
+//    * @namespace Module
+//    * @author Ultron
+//    */
+
+//   const Module = {
+
+//     /**
+//      * Initialize object
+//      * @function init
+//      * @memberof Module
+//      */
+
+//     init() {
+//       this.cache();
+//       this.events();
+//     },
+
+//     /**
+//      * Cache reusable data
+//      * @function cache
+//      * @memberof Module
+//      */
+
+//     cache() {
+//       this.$win = $(window);
+//       this.$doc = $(document);
+//     },
+
+//     /**
+//      * Attach event listeners
+//      * @function events
+//      * @memberof Module
+//      */
+
+//     events() {
+//       this.$doc.on(
+//         'click.ui.custom_namespace',
+//         '.js-trigger',
+//         event => this.myClickHandler(event),
+//       );
+
+//       this.$doc.on(
+//         'resize.ui.custom_namespace',
+//         event => this.myResizeHandler(event),
+//       );
+//     },
+
+//     /**
+//      * Handle click event
+//      * @function myClickHandler
+//      * @memberof Module
+//      */
+
+//     myClickHandler() {
+//       console.log('Click handler'); // eslint-disable-line
+//     },
+
+//     /**
+//      * Handle `window` resize event
+//      * @function myResizeHandler
+//      * @memberof Module
+//      */
+
+//     myResizeHandler() {
+//       console.log('Resize handler'); // eslint-disable-line
+//     },
+//   };
+
+//   Module.init();
+// })(jQuery, window, document);
